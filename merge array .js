935d16ecf186e31ee5be1c5a3a8eaa6f1361b06c
hemantyadav07 <hemanttyadav007@gmail.com>
2022-06-09 12:21:37 +0530

    html program for guess the number

--- merge array .js
+++ merge array .js
@@ -1,16 +1,9 @@
-const calctempamplitudenew = function (t1, t2, t3){
-    
-    const temp = t1.concat(t2, t3);
-    console.log(temp);
-    let max = temp[0];
-    let min = temp[0];
-    for(i = 0; i < temp.length; i++){
-        if(temp[i] > max) max = temp[i];
-        if(temp[i] < min) min = temp[i];
-        
-    }
-    console.log(max, min);
-};
-const amplitudenew = calctempamplitudenew ([3, 7, 4], [5, 8, 6], [11, 9, 10]);
-console.log(amplitudenew);
- 
+<html>
+<head script type="text/javascript" src="merge array.js"></script>
+>        
+        <title>sample program</title>
+        <body>
+            <h1>hello world</>
+        </body>
+    </head>
+</html>
---
+++ script.js
@@ -0,0 +1,13 @@
+/*'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.messagw').textContent = 'Correct Number!';
+
+document.querySelector('number').textContent = 13;
+document.querySelector('.score').textContent
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+*/
+document.querySelector('.check').addEventListener('click'function() {
+    console.log(document.querySelector('guess').value);
+
+});
